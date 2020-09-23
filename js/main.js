989f563d080c578ef39aedb92f4ab71221d48b16
Mir <mirlan.ru@inbox.ru>
2020-09-24 00:16:16 +0500

    pricetime

--- js/main.js
+++ js/main.js
@@ -333,7 +333,13 @@ $(document).on('click', '.rmpwr', function(){
   $('body,html').animate({scrollTop: top}, 0);
   $('.modal-rmpwr').fadeIn('slow','linear');
 });
-
+$(document).on('click', '.fontan-prtf', function(){
+  event.preventDefault();
+  var id = $('.modalyak'),
+  top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 0);
+  $('.modal-fontan-prtf').fadeIn('slow','linear');
+});
 
 
 
@@ -399,3 +405,16 @@ $(document).on('click', '.close', function(){
 
 
 
+
+$(document).on('click', '.more-logo', function(){
+  $('.more-logo-r').toggleClass('left');
+  $('.more-logo-r').toggleClass('pricetime-height');
+});
+$(document).on('click', '.more-style', function(){
+  $('.more-style-r').toggleClass('left');
+  $('.more-style-r').toggleClass('pricetime-height');
+});
+$(document).on('click', '.more-mat', function(){
+  $('.more-mat-r').toggleClass('left');
+  $('.more-mat-r').toggleClass('pricetime-height');
+});
